db5ebad30eb1744ccc1b2edc4ad24319e6cddf3a
Elliot Bouvier <ebouvier@student.42.fr>
2024-11-10 15:43:32 +0000

    merge with main

--- p1/scripts/config_server.sh
+++ p1/scripts/config_server.sh
@@ -5,4 +5,4 @@ sudo apt install -y curl
 
 /usr/local/bin/k3s-uninstall.sh || echo "k3s is not installed"
 
-curl -sfL https://get.k3s.io | K3S_TOKEN="$K_TOKEN" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--node-ip=$SERVER_IP --flannel-iface=eth1" sh -s -
+curl -sfL https://get.k3s.io | K3S_TOKEN="$TOKEN" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--bind-address=$SERVER_IP --flannel-iface=eth1" sh -s -
--- p2/scripts/server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE='644' sh -s - --flannel-iface=eth1 --bind-address=${1}
-
-echo "Initializing apps"
-sleep 5
-
-kubectl apply -f /opt/apps.yml -n apps && echo "Done."
