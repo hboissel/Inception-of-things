8b062711e455956cf330a42bff9e81d47fd11997
Elliot Bouvier <ebouvier@student.42.fr>
2024-11-10 09:08:22 +0000

    switch to alpine linux, way faster

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --write-kubeconfig-mode=644 --node-ip ${1} --node-name ${2} --flannel-iface eth1 --token ${3}
+#!/bin/sh
 
-if systemctl is-active --quiet k3s; then
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - \
+--write-kubeconfig-mode=644 \
+--node-ip ${1} \
+--node-name ${2} \
+--flannel-iface eth1 \
+--token ${3}
+
+if service k3s status | grep -q "started"; then
     echo "K3s server started successfully."
-    sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+    chmod 644 /etc/rancher/k3s/k3s.yaml
 else
     echo "K3s server failed to start. Checking logs..."
-    journalctl -xeu k3s.service
+    cat /var/log/k3s-service.log
 fi
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://${1}:6443 --node-name ${2} --node-ip ${3} --flannel-iface eth1 --token ${4}" sh -s -
