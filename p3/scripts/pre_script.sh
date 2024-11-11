1db17e5d7de435c93e25041e09b330f3743af008
Elliot Bouvier <ebouvier@student.42.fr>
2024-11-11 08:55:41 +0000

    pre_script

---
+++ p3/scripts/pre_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+
+# install docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+#setup current user in docker group (remove the need of sudo)
+sudo usermod -aG docker $USER
+
+docker run hello-world
+
+
+#setup k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
